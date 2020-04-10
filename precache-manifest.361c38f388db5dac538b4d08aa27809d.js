fd4192776209ca92d534882373b0b1db620ee79b
Frithjof Winkelmann <frithjof.winkelmann@iteratec.com>
2020-04-10 16:36:21 +0200

    Updates

---
+++ precache-manifest.361c38f388db5dac538b4d08aa27809d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11ba502e36b39e0449618cfa516663f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "69e037a0bdfe47606094",
+    "url": "/static/css/2.1fba6b52.chunk.css"
+  },
+  {
+    "revision": "3aba2ee917d39764e351",
+    "url": "/static/css/main.ff104cca.chunk.css"
+  },
+  {
+    "revision": "69e037a0bdfe47606094",
+    "url": "/static/js/2.852ccb47.chunk.js"
+  },
+  {
+    "revision": "14f6f4d53b0498c8c9401d62814e6e45",
+    "url": "/static/js/2.852ccb47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3aba2ee917d39764e351",
+    "url": "/static/js/main.304b41ea.chunk.js"
+  },
+  {
+    "revision": "c1ca01838fd0fef55ded",
+    "url": "/static/js/runtime-main.2a146b8e.js"
+  }
+]);
