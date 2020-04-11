839f77726eaad8cf8ff0364453ff8e3b0e5fdbfb
Frithjof Winkelmann <frithjof.winkelmann@iteratec.com>
2020-04-11 12:13:24 +0200

    Updates

---
+++ precache-manifest.7d814d57f40cbe1f91e5960275d99ddc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d3d2f794d4e0d0f341a23a8553950c6",
+    "url": "/note-pwa/index.html"
+  },
+  {
+    "revision": "69e037a0bdfe47606094",
+    "url": "/note-pwa/static/css/2.1fba6b52.chunk.css"
+  },
+  {
+    "revision": "e28986ae7c16719d3a41",
+    "url": "/note-pwa/static/css/main.ff104cca.chunk.css"
+  },
+  {
+    "revision": "69e037a0bdfe47606094",
+    "url": "/note-pwa/static/js/2.852ccb47.chunk.js"
+  },
+  {
+    "revision": "14f6f4d53b0498c8c9401d62814e6e45",
+    "url": "/note-pwa/static/js/2.852ccb47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e28986ae7c16719d3a41",
+    "url": "/note-pwa/static/js/main.d0863df7.chunk.js"
+  },
+  {
+    "revision": "0f33cbf3a4f69a5d19c3",
+    "url": "/note-pwa/static/js/runtime-main.5a667aa7.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4f3db72e528a205c685d6951d19dcb96.js"
+  "/note-pwa/precache-manifest.7d814d57f40cbe1f91e5960275d99ddc.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/note-pwa/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
