15713c2293e2580f4e066ec354f2b02afcbe26cd
Frithjof Winkelmann <frithjof97@web.de>
2020-10-26 22:14:05 +0100

    Switch to github actions

--- ci/deploy.sh
+++
@@ -1,9 +0,0 @@
-git remote remove origin
-git remote add origin git@github.com:Hoff97/note-pwa.git
-
-git fetch
-git branch --set-upstream-to origin/master master
-git fetch origin gh-pages:gh-pages
-git branch --set-upstream-to origin/gh-pages gh-pages
-
-npm run deploy
